12062b0deba670807d31a2bc3add4a31ae057bac
Himeth09 <himeth.jayakody@gmail.com>
2019-09-29 06:22:12 -0400

    Firstish commit

---
+++ CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.currentplacedetailsonmap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MapsActivityCurrentPlace.class);
+        startActivity(intent);
+        finish();
+    }
+}
